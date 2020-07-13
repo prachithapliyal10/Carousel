ed27edded72f3689df7a393b6afbfd9c0da2ae64
Prachi Thapliyal <prathapl@publicisgroupe.net>
2020-07-13 16:21:36 +0530

    First coomit

---
+++ carousel-new.js
@@ -0,0 +1,74 @@
+const carousel = document.querySelector(".carousel");
+//console.log(carousel);
+
+const nextButton = document.querySelector(".right-btn");
+
+const prevButton = document.querySelector(".left-btn");
+
+const slides = [...carousel.children];
+
+let slideWidth = slides[0].getBoundingClientRect().width;   //slide width
+//console.log(slideWidth);
+
+var count=0;
+var slidePerPage = slideWidth/3;
+
+//Position of one image horizontally
+function positionSlides(slides) {
+    for(let index=0; index < slides.length; index++) {
+       // slides[index].style.left = slideWidth * index + "px";
+       slides[index].style.left =  count + "px";
+        count += slidePerPage;
+        
+       // slides[0].style.left = slideWidth * 0 + "px";
+       // slides[1].style.left = slideWidth * 1 + "px";
+       // slides[2].style.left = slideWidth * 2 + "px";
+    }
+}
+
+positionSlides(slides);
+
+nextButton.addEventListener("click", function(){
+    const currentSlide = carousel.querySelector(".active");
+   // var currentOne = currentSlide.style.left;
+    //var cc = currentOne + slideWidth;
+    const nextSlide = currentSlide.nextElementSibling;
+    //console.log(currentSlide, nextSlide);
+    var position = nextSlide.style.left;
+   // position = position * 2;
+    
+    
+    carousel.style.transform = `translateX(-${position})`; // 'translateX(- "+position+")'
+    currentSlide.classList.remove("active");
+    nextSlide.classList.add("active");
+
+    arrowDisable(nextSlide, slides)
+});
+
+prevButton.addEventListener("click", function(){
+    const currentSlide = carousel.querySelector(".active");
+    const prevSlide = currentSlide.previousElementSibling;
+    //console.log(currentSlide, nextSlide);
+    
+    var position = prevSlide.style.left;
+    position = position * 3;
+    
+    carousel.style.transform = `translateX(-${position})`;
+    currentSlide.classList.remove("active");
+    prevSlide.classList.add("active");
+
+    arrowDisable(prevSlide, slides);
+});
+
+function arrowDisable(targetslide, slides) {
+    if(targetslide==slides[0]){
+        prevButton.classList.add("hide");
+        nextButton.classList.remove("hide");
+    } else if(targetslide==slides[slides.length-1]) {
+        prevButton.classList.remove("hide");
+        nextButton.classList.add("hide");
+    } else {
+        prevButton.classList.remove("hide");
+        nextButton.classList.remove("hide");
+    }
+}
